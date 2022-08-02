7aa809eb345592b98c54eb3c690521c6e0897c99
Kenneth Mei <99045224+kazziken@users.noreply.github.com>
2022-08-02 01:10:37 -0400

    nav bar looks ok now

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,68 +1,118 @@
-import React from 'react';
-import { Link, useHistory } from "react-router-dom";
+import React , {useEffect} from 'react'
+import './App.css';
+import { Link, useHistory } from 'react-router-dom';
+import $ from 'jquery';
 
-function Navbar({ setUser }) {
-  let history = useHistory();
+function Navbar({setUser}) {
 
-  function handleLogOut() {
-    fetch("/logout", { method: "DELETE" })
+  function animation(){
+    var tabsNewAnim = $('#navbarSupportedContent');
+    var activeItemNewAnim = tabsNewAnim.find('.active');
+    var activeWidthNewAnimHeight = activeItemNewAnim.innerHeight();
+    var activeWidthNewAnimWidth = activeItemNewAnim.innerWidth();
+    var itemPosNewAnimTop = activeItemNewAnim.position();
+    var itemPosNewAnimLeft = activeItemNewAnim.position();
+    $(".hori-selector").css({
+      "top":itemPosNewAnimTop.top + "px", 
+      "left":itemPosNewAnimLeft.left + "px",
+      "height": activeWidthNewAnimHeight + "px",
+      "width": activeWidthNewAnimWidth + "px"
+    });
+    $("#navbarSupportedContent").on("click","li",function(e){
+      $('#navbarSupportedContent ul li').removeClass("active");
+      $(this).addClass('active');
+      var activeWidthNewAnimHeight = $(this).innerHeight();
+      var activeWidthNewAnimWidth = $(this).innerWidth();
+      var itemPosNewAnimTop = $(this).position();
+      var itemPosNewAnimLeft = $(this).position();
+      $(".hori-selector").css({
+        "top":itemPosNewAnimTop.top + "px", 
+        "left":itemPosNewAnimLeft.left + "px",
+        "height": activeWidthNewAnimHeight + "px",
+        "width": activeWidthNewAnimWidth + "px"
+      });
+    });
+  }
+
+  useEffect(() => {
+    
+    animation();
+    $(window).on('resize', function(){
+      setTimeout(function(){ animation(); }, 500);
+    });
+    
+  }, []);
+
+  let history= useHistory();
+
+  function handleLogOut(){
+    fetch("/logout", {method: "DELETE"})
       .then((r) => {
         if (r.ok) {
           setUser({})
           history.push("/")
         }
       })
-    }
-
-  // const [collapsed, setCollapsed] = useState(false);
-
-  // const toggleCollapsed = () => {
-  //   setCollapsed(!collapsed);
-  // };
-      
-    return (
-    <nav className="navbar navbar-expand-lg navbar-mainbg">
-      <div className="navbar-brand navbar-logo">
-        <Link exact to="/Home" className="nav-link">
-          Home
-        </Link>
-      </div>
-      <div className="nav-item">
-        <Link to="/Profile" className="nav-link" >
-          Profile
-        </Link>
-        </div >
-      <div className="nav-item">
-        <Link to="/Cart" className="nav-link" >
-        Cart
-        </Link>
-      </div >
-      <div className="nav-item">
-        <Link to="/" className="nav-link" onClick={handleLogOut}>
-          Sign Out
-        </Link>
-      </div>
-      <div className="collapse 
-      navbar-collapse"
-      id="navbarSupportedContent">
-        <ul className="navbar-nav ml-auto">
-          <div className="hori-selector">
-            <div className="left"></div>
-            <div className="right"></div>
-          </div>
-        </ul>
-        <li className="nav-item active">
-          <Link to="/Profile" className="nav-link" >
-          <i
-          className="fas
-          fa-tachometer-alt">
-          </i>Profile
-          </Link>
-        </li>
-      </div>
-    </nav>
-    );
   }
 
+  return (
+  <nav className="navbar navbar-expand-lg navbar-mainbg">
+    
+      <Link className="navbar-brand navbar-logo" to="/" exact>
+        
+      </Link>
 
+      <button 
+        className="navbar-toggler"
+        onClick={ function(){
+          setTimeout(function(){ animation(); });
+        }}
+        type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <i className="fas fa-bars text-white"></i>
+      </button>
+ 
+      <div
+        className="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul className="navbar-nav ml-auto">
+            
+            <div className="hori-selector">
+              <div className="left"></div>
+              <div className="right"></div>
+            </div>
+            
+            <li className="nav-item active">
+              <Link className="nav-link" to="/Home" exact>
+                <i 
+                className="fas fa-tachometer-alt">
+                </i>Home
+              </Link>
+            </li>
+
+            <li className="nav-item">
+              <Link className="nav-link" to="/Profile" exact>
+                <i 
+                className="far fa-address-book">
+                </i>Profile
+              </Link> 
+            </li>
+
+            <li className="nav-item">
+              <Link className="nav-link" to="/Cart" exact>
+                <i 
+                className="far fa-clone">
+                </i>Cart
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link className="nav-link" to="/" exact>
+                <i 
+                className="far fa-chart-bar">
+                </i>Logout
+              </Link>
+            </li>
+        </ul>
+      </div>
+  </nav>
+  )
+}
 export default Navbar;
